91bc2c87c4571176eb9d23603ed806c7886ef64a
ProjectBackupRahul <projectbackuprahul@gmail.com>
2020-08-19 00:02:24 +0530

    Addtion of dashboard functionality

--- app.js
+++ app.js
@@ -1,54 +1,102 @@
-const path =  require('path');
-const express = require('express');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
-const exphbs =  require('express-handlebars');
-const passport =  require('passport');
-const session = require('express-session');
-const connectDB =  require("./config/db");
+const path = require('path')
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const connectDB = require('./config/db')
 
-//@ Load Config 
-dotenv.config({path: './config/config.env'});
+// Load config
+dotenv.config({ path: './config/config.env' })
 
-// @ Passport config 
-
+// Passport config
 require('./config/passport')(passport)
-connectDB();
-const app = express();
 
-// @ Use of morgan for devlopment login.
+connectDB()
 
-if (process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'))
+const app = express()
+
+// Body parser
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+// Method override
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method
+      delete req.body._method
+      return method
+    }
+  })
+)
+
+// Logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
 }
- // @ using handlebars middlewares 
- app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}));
- app.set('view engine', '.hbs');
 
- // @ Express-Session middleware config 
+// Handlebars Helpers
+const {
+  formatDate,
+  stripTags,
+  truncate,
+  editIcon,
+  select,
+} = require('./helpers/hbs')
 
- app.use(session({
+// Handlebars
+app.engine(
+  '.hbs',
+  exphbs({
+    helpers: {
+      formatDate,
+      stripTags,
+      truncate,
+      editIcon,
+      select,
+    },
+    defaultLayout: 'main',
+    extname: '.hbs',
+  })
+)
+app.set('view engine', '.hbs')
+
+// Sessions
+app.use(
+  session({
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: false,
-   
-  }))
-  
- // @ Passport middlewares
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+)
 
- app.use(passport.initialize());
- app.use(passport.session());
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
 
- // @ Config Staic folder
- app.use(express.static(path.join(__dirname, 'public')));
+// Set global var
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null
+  next()
+})
 
- // @ using all the routes 
- 
- app.use('/', require('./routes/index'));
- app.use('/auth', require('./routes/auth'));
- 
-// @ App PORT 
-const PORT = process.env.PORT || 5000;
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
-// @ App Listen 
-app.listen(PORT, console.log(` The Server is up and running ${process.env.NODE_ENV}  mode on port  ${PORT}`))    
+// Routes
+app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
+
+const PORT = process.env.PORT || 3000
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`) )
